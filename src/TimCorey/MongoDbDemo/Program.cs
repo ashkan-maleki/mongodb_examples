aab04342d7b4b70c717948d4681ac6d4aed17a0c
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-27 16:58:58 +0330

    Store a person in db and retrieve it.

--- src/TimCorey/MongoDbDemo/Program.cs
+++ src/TimCorey/MongoDbDemo/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using MongoDB.Driver;
+
+string connectionString = "mongodb://127.0.0.1:27017";
+string databaseName = "tim_corey";
+string collectionName = "people";
+
+MongoClient client = new(connectionString);
+IMongoDatabase db = client.GetDatabase(databaseName);
+IMongoCollection<PersonModel> collection = db.GetCollection<PersonModel>(collectionName);
+
+PersonModel personModel = new() {FirstName = "Johnny", LastName = "Cage"};
+
+await collection.InsertOneAsync(personModel);
+
+IAsyncCursor<PersonModel> people = await collection.FindAsync(_ => true);
+
+foreach (PersonModel person in people.ToList())
+{
+    Console.WriteLine($"{person.Id}: {person.FirstName} {person.LastName}");
+}
